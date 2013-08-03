b5fc15688871666da5828ae0d9f69007152ce849
Perka <mrperkarlsson@gmail.com>
2013-08-03 17:01:59 +0200

    build.sh update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-export CROSS_COMPILE=~/kernel/toolchains/gcc-linaro-arm/bin/arm-linux-gnueabihf-
+export export CROSS_COMPILE=~/kernel/toolchains/arm-unknown-linux-gnueabi-4.7.4/bin/arm-gnueabi-
 make mrproper
 make shark-e_defconfig
 time make -j3
+echo ""
+echo "Done"
