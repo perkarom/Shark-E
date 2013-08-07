281dd3dc25fb13d6588c26f5c45cbb826e258b0b
Perka <mrperkarlsson@gmail.com>
2013-08-07 03:06:04 +0200

    config

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export export CROSS_COMPILE=~/kernel/toolchains/arm-unknown-linux-gnueabi-4.7.4/bin/arm-gnueabi-
+export CROSS_COMPILE=/home/perka/kernel/toolchains/arm-unknown-linux-gnueabi-4.7.4/bin/arm-gnueabi-
 make mrproper
 make shark-e_defconfig
 time make -j3
